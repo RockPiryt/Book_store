b3cd378e64e2f7fb3676981891c1d11cd76368da
Paulina Kimak <paulina.kimak.oferty@gmail.com>
2023-09-07 13:55:38 +0200

    'RockPiryt'- add new models,
    - add Country, Address, Author models,
    - create 3 types of relationships (one to one, one to many, many to many)
    
    - use Meta class to make changes in Django Admin Section,

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Book, Author, Address, Country
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    # readonly_fields = ("slug",) # show only filed, can't edit value (Change book option), can't use with prepopulated!!!!
+    prepopulated_fields = {"slug":("title",)}# prepopulated field slug with slugify title (Add new book option)
+    list_filter = ("author", "rating",) # filter data by column name
+    list_display = ("title", "author",)
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
